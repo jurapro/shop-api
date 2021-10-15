51bf99ab0ee9873ffee2e86cafdf1519560a1bae
Илья <61906559+kul2002il@users.noreply.github.com>
2021-10-15 10:50:12 +0700

    Добавление функционала администратора. (#6)
    * Пользователи в БД теперь сеются с правильными ролями. (Решение бага.)
    
    * Добавлен функционал администратора.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductAddRequest;
+use App\Http\Requests\ProductEditRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
@@ -11,5 +13,34 @@ class ProductController extends Controller
     public function index()
     {
         return ProductResource::collection(Product::all());
+    }
+
+    public function add(ProductAddRequest $request)
+    {
+        $product = Product::create($request->all());
+        return [
+            'data' => [
+                'id' => $product->id,
+                'message' => 'Product added',
+            ]
+        ];
+    }
+
+    public function edit(Product $product, ProductEditRequest $request)
+    {
+        $product->update($request->all());
+        return [
+            'data' => $product,
+        ];
+    }
+
+    public function delete(Product $product)
+    {
+        $product->delete();
+        return [
+            'data' => [
+                'message' => 'Product removed',
+            ]
+        ];
     }
 }
